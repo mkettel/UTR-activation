9f16e35779326bc863bbe191e06aeb8ba0fdcd10
Matthew Kettelkamp <matthew@pxlagency.com>
2025-01-06 09:40:54 -0800

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-      // Warning: this allows all ESLint errors and warnings
       ignoreDuringBuilds: true,
     },
     typescript: {
-      // Warning: this allows all TypeScript errors
       ignoreBuildErrors: true,
-    },
+    }
   }
   
-  module.exports = nextConfig
+  export default nextConfig
